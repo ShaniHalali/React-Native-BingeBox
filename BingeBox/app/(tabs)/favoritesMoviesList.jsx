935f87b8f730c365aa75ff9c3b8e384644c5aab9
Shani Halali <shanihlli1234@gmail.com>
2025-12-25 15:27:19 +0200

    Add: favorites screen inital

--- BingeBox/app/(tabs)/favoritesMoviesList.jsx
+++ BingeBox/app/(tabs)/favoritesMoviesList.jsx
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Platform } from 'react-native'
 import React from 'react'
+import { Colors } from "../../constants/Color";
+import ThemedView from '../../components/ThemedView'
+import moviesList from './movies/moviesList'
+import ThemedLogo from '../../components/ThemedLogo'
+import ThemedText from '../../components/ThemeText'
+import Spacer from '../../components/Spacer'
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addFavoriteMovie,
+  removeFavoriteMovie,
+} from "../../redux/slices/favoritesSlice";
+
+const fontFamilyPlatform = Platform.OS === "ios" ? "Poppins-Bold" : "Poppins-Bold";
+
 
 const favoritesMoviesList = () => {
+  const dispatch = useDispatch();
+  const favoritesMovies = useSelector((state) => state.favorites);
+  console.log("Favorites movies REDUX: ", favoritesMovies);
+  const favoritesMoviesLength = favoritesMovies.length;
+  console.log("Favorites movies REDUX size: ",favoritesMoviesLength)
+  const isSavedMovies = favoritesMoviesLength > 0;
+
+  const getScreenTitle = () =>  {
+    if(isSavedMovies) {
+      return "Binge your favorites movies!"
+    }
+    return "You didn't save any movies yet!"
+  }
+
   return (
-    <View>
-      <Text>favoritesMoviesList</Text>
-    </View>
+    <ThemedView mode={'moviesList'} >
+        <ThemedLogo style={{height: 150 , alignSelf: 'center'}}/>
+         <ThemedText style={[styles.title, { alignSelf: 'center' }]}>{getScreenTitle()}</ThemedText>
+          <Spacer/>
+          {/* Favorites movies list*/}
+
+    </ThemedView>
+
   )
 }
 
 export default favoritesMoviesList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    title: {
+      fontWeight: "bold",
+      fontSize: 25,
+      fontFamily: fontFamilyPlatform,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    },
+    error: {
+      color: Colors.warning,
+      padding: 10,
+      backgroundColor: "#f5c1c8",
+      borderColor: Colors.warning,
+      borderWidth: 1,
+      borderRadius: 6,
+      marginHorizontal: 10,
+    },
+    buttons: {
+      alignSelf: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 20,
+      width: "70%",
+      alignItems: "center",
+    },
+  
+    btnText: {
+      color: "white",
+      fontSize: 24,
+      fontWeight: "bold",
+      fontFamily: fontFamilyPlatform,
+    },
+})
--- BingeBox/app/(tabs)/movies/moviesList.jsx
+++ BingeBox/app/(tabs)/movies/moviesList.jsx
@@ -1,101 +1,105 @@
-import { StyleSheet, Text, View, Platform, Image, ImageBackground, useColorScheme } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { Colors } from '../../../constants/Color'
-import ThemedView from '../../../components/ThemedView'
-import ThemedText from '../../../components/ThemeText'
-import ThemedLogo from '../../../components/ThemedLogo'
-import Spacer from '../../../components/Spacer'
-import MoviesFlatList from '../../../components/MoviesFlatList'
-import { fetchMovies } from '../../../services/mappers/api/moviesApi'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Image,
+  ImageBackground,
+  useColorScheme,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { Colors } from "../../../constants/Color";
+import ThemedView from "../../../components/ThemedView";
+import ThemedText from "../../../components/ThemeText";
+import ThemedLogo from "../../../components/ThemedLogo";
+import Spacer from "../../../components/Spacer";
+import MoviesFlatList from "../../../components/MoviesFlatList";
+import { fetchMovies } from "../../../services/mappers/api/moviesApi";
 
-const fontFamilyPlatform =  Platform.OS === 'ios' ? 'Poppins-Bold' : 'Poppins-Bold';
+const fontFamilyPlatform = Platform.OS === "ios" ? "Poppins-Bold" : "Poppins-Bold";
 
 const moviesList = () => {
-
   useEffect(() => {
     const loadMovies = async () => {
-      try{
-        const moviesFetchedList = await fetchMovies()
-        console.log('movies fetched:', moviesFetchedList)
-        setMovies(moviesFetchedList)
-
-      } catch(error) {
-        console.error('Failed to fetch movies:', error)
-        setError(error)
+      try {
+        const moviesFetchedList = await fetchMovies();
+        console.log("movies fetched:", moviesFetchedList);
+        setMovies(moviesFetchedList);
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+        setError(error);
       }
-    }
-    loadMovies()
-  }, [])
+    };
+    loadMovies();
+  }, []);
 
-  const [moviesList, setMovies] = useState(null)
-  const [error, setError] = useState(null)
+  const [moviesList, setMovies] = useState(null);
+  const [error, setError] = useState(null);
 
   return (
-    <ThemedView mode={'moviesList'} style={[styles.container ]}>
-        <ThemedLogo style={{height: 150 , alignSelf: 'center'}}/>
-         <ThemedText style={[styles.title, { alignSelf: 'center' }]}>Ready to Binge?</ThemedText>
-         <ThemedText style={[styles.title, { alignSelf: 'center' }]}>Heres whats hot right now</ThemedText>
-        <Spacer/>
-        {/* Failed to fetch movies*/}
-        {error && <Text style={styles.error}>{error}</Text>}
+    <ThemedView mode={"moviesList"} style={[styles.container]}>
+      <ThemedLogo style={{ height: 150, alignSelf: "center" }} />
+      <ThemedText style={[styles.title, { alignSelf: "center" }]}>
+        Ready to Binge?
+      </ThemedText>
+      <ThemedText style={[styles.title, { alignSelf: "center" }]}>
+        Heres whats hot right now
+      </ThemedText>
+      <Spacer />
+      {/* Failed to fetch movies*/}
+      {error && <Text style={styles.error}>{error}</Text>}
 
-        {/* Loading indicator*/}
-        {!moviesList && !error && (
-          <ThemedText style={{marginTop: 20 }}>Loading Movies...</ThemedText>
-        )}
+      {/* Loading indicator*/}
+      {!moviesList && !error && (
+        <ThemedText style={{ marginTop: 20 }}>Loading Movies...</ThemedText>
+      )}
 
-        {/* Uploaded Movies successfully */}
-        {moviesList && (
-        <MoviesFlatList movies={moviesList}/>
-        )}
-
+      {/* Uploaded Movies successfully */}
+      {moviesList && <MoviesFlatList movies={moviesList} />}
     </ThemedView>
-  )
-}
+  );
+};
 
-export default moviesList
+export default moviesList;
 
 const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 25,
-        fontFamily: fontFamilyPlatform,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5, 
-        
-    },
-    error: {
-        color: Colors.warning,
-        padding: 10,
-        backgroundColor: '#f5c1c8',
-        borderColor: Colors.warning,
-        borderWidth: 1,
-        borderRadius: 6,
-        marginHorizontal: 10,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-      buttons: {
-        alignSelf: 'center',
-        flexDirection: 'row',
-        justifyContent: "space-between",
-        paddingHorizontal: 20,
-        width: "70%",
-        alignItems: 'center',
-      },
+  title: {
+    fontWeight: "bold",
+    fontSize: 25,
+    fontFamily: fontFamilyPlatform,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  error: {
+    color: Colors.warning,
+    padding: 10,
+    backgroundColor: "#f5c1c8",
+    borderColor: Colors.warning,
+    borderWidth: 1,
+    borderRadius: 6,
+    marginHorizontal: 10,
+  },
+  buttons: {
+    alignSelf: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    width: "70%",
+    alignItems: "center",
+  },
 
   btnText: {
-  color: 'white',
-  fontSize: 24, 
-  fontWeight: 'bold',
-  fontFamily: fontFamilyPlatform,
-},
-
-
-})
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    fontFamily: fontFamilyPlatform,
+  },
+});
